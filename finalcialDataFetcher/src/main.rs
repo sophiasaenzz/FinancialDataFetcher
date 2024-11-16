cefd85e6cfc00ddc7452327a84028c41556b5740
Sophia Saenz <94822977+sophiasaenzz@users.noreply.github.com>
2024-11-16 18:51:07 +0000

    api is working

--- finalcialDataFetcher/src/main.rs
+++ finalcialDataFetcher/src/main.rs
@@ -1,8 +1,8 @@
+use std::io::Write;
 //build rust application that periodically fetches and records the pricing data of bitcoin, ethereum and the SP500 index
-//use ureq::AgentBuilder; //ureq for making http requests
 use std::time::Duration;
 use serde::{Deserialize, Serialize}; //serde for parsing json data into rust structs
-use std::fs::File; //file for writing to files
+use std::fs::OpenOptions; //file for writing to files
 use std::error::Error;
 use serde_json::Value;
 
@@ -12,32 +12,21 @@ use serde_json::Value;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Bitcoin {
     price: f64,
-    //time: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Ethereum {
     price: f64,
-    //time: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SP500 {
     price: f64,
-    //time: String,
 }
 
 //define a pricing trait with the following methods
 //fetch_price() fetches the latest price from a API
 //save_to_file() saves the fetched price to a file
-
-/* 
-#[derive(Debug)]
-enum ApiResult {
-    Success(f64),
-    ApiError(String),
-    NetworkError(String),    
-} */
 
 trait Pricing {
     fn fetch_price(&self) -> Result<f64, Box<dyn Error>>;
@@ -59,7 +48,7 @@ impl Pricing for Bitcoin {
                     let data: Value = serde_json::from_str(&body)?;
                     
                     // Extract the price from the JSON
-                    if let Some(price) = data["current_price"].as_f64() {
+                    if let Some(price) = data["bpi"]["USD"]["rate_float"].as_f64() {
                         Ok(price)
                     } else {
                         Err("Failed to extract Bitcoin price from response".into())
@@ -77,8 +66,15 @@ impl Pricing for Bitcoin {
         match self.fetch_price() {
             Ok(price) => {
                 let bitcoin = Bitcoin { price };
-                let file = File::create("bitcoin.txt").unwrap();
-                serde_json::to_writer(file, &bitcoin).unwrap();
+
+                let mut file = OpenOptions::new()
+                    .write(true)
+                    .append(true)
+                    .open("bitcoin.txt")
+                    .unwrap();
+
+                file.write_all(bitcoin.price.to_string().as_bytes()).unwrap();
+
             }
             Err(e) => eprintln!("Failed to fetch price: {}", e),
         }
@@ -102,10 +98,10 @@ impl Pricing for Ethereum {
                     let data: Value = serde_json::from_str(&body)?;
                     
                     // Extract the price from the JSON
-                    if let Some(price) = data["Ethereum"]["USD"]["rate_float"].as_f64() {
-                        Ok(price)
+                    if let Some(ethereum_data) = data[1]["current_price"].as_f64() {
+                        Ok(ethereum_data)
                     } else {
-                        Err("Failed to extract Bitcoin price from response".into())
+                        Err("Failed to extract ethereum price from response".into())
                     }
                 } else {
                     Err(format!("Unexpected status: {}", response.status()).into())
@@ -120,8 +116,15 @@ impl Pricing for Ethereum {
         match self.fetch_price() {
             Ok(price) => {
                 let ethereum = Ethereum { price };
-                let file = File::create("ethereum.txt").unwrap();
-                serde_json::to_writer(file, &ethereum).unwrap();
+                //let file = File::create("ethereum.txt").unwrap();
+                
+                let mut file = OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open("ethereum.txt")
+                .unwrap();
+
+                file.write_all(ethereum.price.to_string().as_bytes()).unwrap();
             }
             Err(e) => eprintln!("Failed to fetch price: {}", e),
         }
@@ -143,10 +146,10 @@ impl Pricing for SP500 {
                     let data: Value = serde_json::from_str(&body)?;
                     
                     // Extract the price from the JSON
-                    if let Some(price) = data["Ethereum"]["USD"]["rate_float"].as_f64() {
+                    if let Some(price) = data[3]["current_price"].as_f64() {
                         Ok(price)
                     } else {
-                        Err("Failed to extract Bitcoin price from response".into())
+                        Err("Failed to extract sp500 price from response".into())
                     }
                 } else {
                     Err(format!("Unexpected status: {}", response.status()).into())
@@ -156,12 +159,20 @@ impl Pricing for SP500 {
         }
     }
 
-    fn save_to_file(&self) {
+    fn save_to_file(&self) {        
         match self.fetch_price() {
+            //create file            
             Ok(price) => {
-                let ethereum = Ethereum { price };
-                let file = File::create("SP500.txt").unwrap();
-                serde_json::to_writer(file, &ethereum).unwrap();
+
+                //let mut file = std::fs::File::create("sp500.txt").unwrap();
+                let sp500 = Ethereum { price };
+                let mut file = OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open("sp500.txt")
+                .unwrap();
+
+            file.write_all(sp500.price.to_string().as_bytes()).unwrap();
             }
             Err(e) => eprintln!("Failed to fetch price: {}", e),
         }
@@ -169,26 +180,13 @@ impl Pricing for SP500 {
 }
 
 
-
-
-/*enum Common {
-    Bitcoin(Bitcoin),
-    Ethereum(Ethereum),
-    SP500(SP500),
-  } */
-  
-
 fn main() {
-    //println!("Hello, world!");
-
     //create a vector containing instances of the Bitcoin, Ethereum and SP500 structs
     let pricing: Vec<Box<dyn Pricing>> = vec![
         Box::new(Bitcoin { price: 0.0 }),
-        //Box::new(Ethereum { price: 0.0 }),
-        //Box::new(SP500 { price: 0.0 }),
+        Box::new(Ethereum { price: 0.0 }),
+        Box::new(SP500 { price: 0.0 }),
     ];
-
-
 
     // Periodically fetch and save prices
     loop {
